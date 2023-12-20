6f1c9e92c3c80ef9a38b08aca8cafff90a4d77bd
roxanachiriac2002 <roxana_chiriac2002@yahoo.com>
2023-12-20 17:36:40 +0200

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+
+namespace Chiriac_Roxana_Lab7;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    [Obsolete]
+    private void OnButtonClicked(object sender, EventArgs e)
+    {
+        // Change the background color of the button when clicked
+        CounterBtn.BackgroundColor = Color.FromHex("#FF00FF"); // Use the color hex code or any other color representation
+    }
+}
+
+
